65ad9a5ecc0c0a7c0ee2752656d515d578db90bf
Lexioumon <xiaomeng.liu@sigmatechnology.se>
2022-03-31 11:03:00 +0800

    Initial Update

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+//数组传参
+    //二维
+        void (int *arr,[]){}
+        
+    //三维
+        void (int (*arr)[10], size_type sizeRow){}
+        int arr
+    //标准库的遍历
+        void printValues (const int *begin, const int *end)
+
+int main() {
+    cout << "Hello, World!" << std::endl;
+    return 0;
+}
